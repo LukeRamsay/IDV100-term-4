f8b870ff6ebfea00e19db0675e9bcf14589dde6f
DanielSivewright <43437673+DanielSivewright@users.noreply.github.com>
2018-11-02 17:31:35 +0200

    Add files via upload

---
+++ Sign Up + Animation/js/signUp.js
@@ -0,0 +1,8 @@
+function checkInfo(){
+    var form = document.getElementById('form');
+    
+    if (box.value === ""){
+        alert('Please fill in your information');
+        return false;
+    }
+}
